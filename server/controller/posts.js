5920819b4691e0cc6177945caab748f9754214b0
Prithviraj P <51220476+Tesseract99@users.noreply.github.com>
2023-04-02 17:07:03 +0530

    Update posts.js
    fixed: req -> res

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -6,7 +6,7 @@ export const getPosts = async (req, res) => {
     const postMessages = await PostMessage.find();
     res.status(200).json(postMessages);
   } catch (err) {
-    req.status(404).json({ message: err.message });
+    res.status(404).json({ message: err.message });
   }
 };
 
